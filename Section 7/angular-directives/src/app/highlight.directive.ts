498cd75c50b0db7bcf8bdb84cbedb7dd2655d46d
HillLuke <lukehilldev@gmail.com>
2020-12-13 20:39:40 +0000

    Add cusotmisable default/highlight colours using property binding.

--- Section 7/angular-directives/src/app/highlight.directive.ts
+++ Section 7/angular-directives/src/app/highlight.directive.ts
@@ -1,23 +1,30 @@
-import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent'; 
+  @Input() defaultColor: string = 'transparent';
+  @Input() hightlightColor: string = 'blue';
+
+  @HostBinding('style.backgroundColor') backgroundColor: string; 
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+    this.backgroundColor = this.defaultColor;
+  }
 
   @HostListener('mouseenter') mouseenter(eventData: Event){
     // bellow can be used if not using @HostBinding
     //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.hightlightColor;
   }
 
   
   @HostListener('mouseleave') mouseleave(eventData: Event){
     //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 
 
