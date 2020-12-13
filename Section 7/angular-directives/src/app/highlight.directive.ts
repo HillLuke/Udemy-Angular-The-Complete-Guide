dbc4e9c3f1359f2bc93827e5aa05f74221edcd31
HillLuke <lukehilldev@gmail.com>
2020-12-13 20:20:18 +0000

    Add HostBinding to directive

--- Section 7/angular-directives/src/app/highlight.directive.ts
+++ Section 7/angular-directives/src/app/highlight.directive.ts
@@ -1,19 +1,23 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent'; 
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   @HostListener('mouseenter') mouseenter(eventData: Event){
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    // bellow can be used if not using @HostBinding
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = 'blue';
   }
 
   
   @HostListener('mouseleave') mouseleave(eventData: Event){
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
   }
 
 
