9624e3c8fc7e382ac8d129e096fce0027c7d295c
HillLuke <lukehilldev@gmail.com>
2021-01-31 17:03:57 +0000

    Add some basic components for routing

--- Section 11 - Changing Pages with Routing/routing/src/app/app.module.ts
+++ Section 11 - Changing Pages with Routing/routing/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { AdminsComponent } from './admins/admins.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    AdminsComponent
   ],
   imports: [
     BrowserModule
